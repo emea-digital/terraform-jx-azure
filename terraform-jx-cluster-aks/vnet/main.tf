120b08b642fb551a6fa7c3ef29b31e30e0c10e0a
Senim Elhouar <Senim.ELHOUAR@loreal.com>
2021-03-10 11:09:17 +0100

    fix: use network name directly

--- terraform-jx-cluster-aks/vnet/main.tf
+++ terraform-jx-cluster-aks/vnet/main.tf
@@ -11,6 +11,6 @@ resource "azurerm_subnet" "cluster_subnet" {
   count = var.create_vn != true ? 0 : 1
   name                 = var.subnet_name
   resource_group_name  = var.resource_group
-  virtual_network_name = azurerm_virtual_network.cluster.name
+  virtual_network_name = var.network_name
   address_prefixes     = [var.subnet_cidr]
 }
